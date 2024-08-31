c7ec95a1e5db481c85e93eba50885513e2d89003
Info03Nation <info.nation.03@gmail.com>
2024-08-31 18:26:11 +0000

    added html and css
    sample site

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-  return "Hello sample"
+  return render_template('Home.html')
 
 # print(__name__)
 if __name__ == "__main__":
